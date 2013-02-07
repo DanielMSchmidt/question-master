6f9154df91307277733b6bb0060cc9d33cdddc69
Daniel Schmidt <dschmidt@weluse.de>
2013-02-07 10:54:25 +0100

    init

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+QuestionMaster::Application.config.session_store :cookie_store, key: '_question-master_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# QuestionMaster::Application.config.session_store :active_record_store
